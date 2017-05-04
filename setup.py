55fb333d1cefcda4e7b130b2e211534b4391dcd7
Kye Morton <kyemorton@hotmail.com>
2017-05-05 01:28:44 +1000

    Runs successfully!

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
-d = generate_distutils_setup(
+# ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
     packages=['rqt_mc_tuner'],
     package_dir={'': 'src'},
-    scripts=['scripts/rqt_mc_tuner']
+    requires=['mavros', 'mavros_msgs', 'rospy']
 )
+
+setup(**setup_args)
